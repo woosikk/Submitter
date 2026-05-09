9c8779199c25a54a91eedc176ea09831323cc5b6
Woosik Kang <woosik.kang@drexel.edu>
2026-05-09 01:19:28 -0400

    Update setup.py
    Following the updates on submitter.py, increased the subsub-version by one (1), and updated the metadata to include the author info of these commits.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 with open("README.md", "r") as fh:
@@ -10,12 +10,12 @@ with open("README.md", "r") as fh:
 setup(
     name='submitter',
     version=__version__,
-    author='Steve Sclafani, Mike Richman',
+    author='Steve Sclafani, Mike Richman, Woosik Kang',
     author_email='mike.d.richman@gmail.com',
     packages = ['submitter'],
     description='Job submission helper developed at UMD',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='github.com/ssclafani949/Submitter',
+    url='github.com/woosikk/Submitter',
     install_requires=['numpy'],
 )
